0a488ab977ce94066f5d1a2801dbdccd55103c76
shreyas23sk <shreyas23.sk@gmail.com>
2023-04-23 13:51:13 +0530

    Added node definition

--- structs.h
+++ structs.h
@@ -12,3 +12,11 @@ struct Rect {
     Point top_left;
     Point top_right;
 };
+
+// page 4, sec 3.1
+typedef struct Node* NODE;
+struct Node {
+    Rect MBR; // minimum bounding rectangle for all the child nodes of this entry
+    NODE child;
+    int LHV; // largest hilbert value of data rectangles of the subtree (NOT MBR)
+};
