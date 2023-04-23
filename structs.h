94fdabab46f0c3df8a810379e50dd200eef4db38
shreyas23sk <shreyas23.sk@gmail.com>
2023-04-23 13:37:45 +0530

    Point and Rect

---
+++ structs.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Point Point;
+struct Point{
+    int x;
+    int y;
+}
+
+typedef struct Rect Rect;
+struct Rect {
+    Point top_left;
+    Point top_right;
+}
