a97eebdaf56fd0368e8c5926a060880bbed986bf
Roshan <roshanbagla1234@gmail.com>
2023-04-29 16:24:44 +0530

    added some code for AdjustTree

--- insertion.c
+++ insertion.c
@@ -1,8 +1,14 @@
 #include "structs.h"
+#include <limits.h>
+#define MIN(a, b) a < b ? a : b
+#define MAX(a, b) a > b ? a : b
 
-int count_entries(NODE n) {
-    for(int i = 0; i < 4; i++) {
-        if(n->all_entries[i] == NULL) return i; 
+int count_entries(NODE n)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (n->all_entries[i] == NULL)
+            return i;
     }
     return 4;
 }
@@ -35,39 +41,49 @@ NODE chooseLeaf(HRT ht, Rect r, int h)
 
 // if handling insertion of leaf node, calculate hv when calling function, call with n = NULL
 // else feed in LHV of non leaf node, and treat n as the child node of new entry to be inserted
-NODE handleOverflow(NODE L, NODE n, Rect r, int h){
+NODE handleOverflow(NODE L, NODE n, Rect r, int h)
+{
 
     // Entry to be inserted
-    ENTRY new_entry = (ENTRY) malloc(sizeof(Entry));
+    ENTRY new_entry = (ENTRY)malloc(sizeof(Entry));
     new_entry->child = n;
     new_entry->LHV = h;
     new_entry->MBR = r;
 
-    ENTRY* s = L->parent->all_entries;
+    ENTRY *s = L->parent->all_entries;
     int no_of_entries = 1; // node to be inserted;
     int no_of_nodes = 0;
 
-    for(int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         no_of_entries += count_entries(s[i]->child);
     }
 
-    for(int i = 0; i < 4; i++) {
-        if(s[i] == NULL) no_of_nodes = i;
+    for (int i = 0; i < 4; i++)
+    {
+        if (s[i] == NULL)
+            no_of_nodes = i;
     }
 
-    ENTRY* e_arr = (ENTRY) malloc(sizeof(ENTRY) * no_of_entries);
+    ENTRY *e_arr = (ENTRY)malloc(sizeof(ENTRY) * no_of_entries);
 
     int i = 0, j = 0, k = 0; // i - counter for e_arr pointer,
     bool new_entry_inserted = false;
 
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-            if(s[i]->child->all_entries[j] != NULL) {
-                if(new_entry_inserted == false) {
-                    if(new_entry->LHV <= s[i]->child->all_entries[j]->LHV) {
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (s[i]->child->all_entries[j] != NULL)
+            {
+                if (new_entry_inserted == false)
+                {
+                    if (new_entry->LHV <= s[i]->child->all_entries[j]->LHV)
+                    {
                         e_arr[k] = new_entry;
                         new_entry_inserted = true;
                         k++;
+                        j--;
                         continue;
                     }
                 }
@@ -76,8 +92,9 @@ NODE handleOverflow(NODE L, NODE n, Rect r, int h){
             }
         }
     }
-    
-    if(no_of_entries <= (no_of_nodes) * 4) {
+
+    if (no_of_entries <= (no_of_nodes)*4)
+    {
         // evenly distribute existing entries into existing nodes
         // note :- av can never be less than 2 since m is 2
         int av = (no_of_entries) / (no_of_nodes);
@@ -85,23 +102,30 @@ NODE handleOverflow(NODE L, NODE n, Rect r, int h){
 
         int j = 0;
         int k = 0; // current entry in parent node
-        for(int i = 0; i < no_of_entries; i++) {
+        for (int i = 0; i < no_of_entries; i++)
+        {
             s[k]->child->all_entries[j] = e_arr[i];
-            if(e_arr[i]->child != NULL) e_arr[i]->child->parent = L;
+            if (e_arr[i]->child != NULL)
+                e_arr[i]->child->parent = L;
             j++;
-            if(j >= av) {
-                if(rem > 0 && j < 4) rem--;
-                else {
+            if (j >= av)
+            {
+                if (rem > 0 && j < 4)
+                    rem--;
+                else
+                {
                     k++;
                     j = 0;
                 }
             }
         }
 
-        return NULL; 
-    } else {
+        return NULL;
+    }
+    else
+    {
         // create new node
-        NODE new_node = (NODE) malloc(sizeof(struct Node));
+        NODE new_node = (NODE)malloc(sizeof(struct Node));
         // evenly distribute all entries into all 5 nodes
 
         int av = (no_of_entries) / (no_of_nodes);
@@ -109,13 +133,18 @@ NODE handleOverflow(NODE L, NODE n, Rect r, int h){
         int i;
         int j = 0;
         int k = 0; // current entry in parent node
-        for(i = 0; i < no_of_nodes * av ; i++) {
+        for (i = 0; i < no_of_nodes * av; i++)
+        {
             s[k]->child->all_entries[j] = e_arr[i];
-            if(e_arr[i]->child != NULL) e_arr[i]->child->parent = L;
+            if (e_arr[i]->child != NULL)
+                e_arr[i]->child->parent = L;
             j++;
-            if(j >= av) {
-                if(rem > 0 && j < 4) rem--;
-                else {
+            if (j >= av)
+            {
+                if (rem > 0 && j < 4)
+                    rem--;
+                else
+                {
                     k++;
                     j = 0;
                 }
@@ -123,45 +152,104 @@ NODE handleOverflow(NODE L, NODE n, Rect r, int h){
         }
 
         j = 0;
-        while(i < no_of_entries) {
+        while (i < no_of_entries)
+        {
             new_node->all_entries[j] = e_arr[i];
-            i++; j++;
+            i++;
+            j++;
         }
 
         return new_node;
     }
 }
 
+// void checkSplit(NODE leaf, Rect r, int h)
+// {
+
+// }
+
+void AdjustTree(NODE S)
+{
+    if (S == NULL)
+        return;
+    else
+    {
+        NODE P = S->parent;
+        for (int i = 0; i < 4; i++)
+        {
+            ENTRY e = P->all_entries[i];
+            int minx = INT_MAX, maxx = INT_MIN, miny = INT_MAX, maxy = INT_MIN, lhv = 0;
+            if (e != NULL)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    ENTRY ec = e->child->all_entries[j];
+                    if (ec != NULL)
+                    {
+                        minx = MIN(minx, ec->MBR.bottom_left.x);
+                        miny = MIN(miny, ec->MBR.bottom_left.y);
+                        maxx = MAX(maxx, ec->MBR.top_right.x);
+                        maxy = MAX(maxy, ec->MBR.top_right.y);
+                        lhv = MAX(lhv, ec->LHV);
+                    }
+                }
+            }
+            Rect *new_MBR = (Rect *)malloc(sizeof(Rect));
+            new_MBR->bottom_left.x = minx;
+            new_MBR->bottom_left.y = miny;
+            new_MBR->top_right.x = maxx;
+            new_MBR->top_right.y = miny;
+            e->MBR = *new_MBR;
+            e->LHV = lhv;
+        }
+        AdjustTree(P->parent);
+    }
+}
 
 void insert(HRT ht, Rect r)
 {
     int h = calculate_hilbert_value(r);
     NODE leaf = chooseLeaf(ht, r, h);
-    if(leaf->all_entries[3]==NULL){
-        for(int i=0;i<4;i++){
-            if(leaf->all_entries[i]!=NULL && leaf->all_entries[i]->LHV < h){
+    if (leaf->all_entries[3] == NULL)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (leaf->all_entries[i] != NULL && leaf->all_entries[i]->LHV < h)
+            {
                 continue;
             }
-            else{
-                if(leaf->all_entries[i]==NULL){
-                    leaf->all_entries[i]->MBR=r;
-                    leaf->all_entries[i]->LHV=h;
+            else
+            {
+                if (leaf->all_entries[i] == NULL)
+                {
+                    leaf->all_entries[i]->MBR = r;
+                    leaf->all_entries[i]->LHV = h;
                 }
-                else{
-                    ENTRY temp=leaf->all_entries[i];
-                    leaf->all_entries[i]->MBR=r;
-                    leaf->all_entries[i]->LHV=h;
-                    for(int j=i+1;j<4;j++){
-                        ENTRY temp2=leaf->all_entries[j];
-                        leaf->all_entries[j]=temp;
-                        temp=temp2;
+                else
+                {
+                    ENTRY temp = leaf->all_entries[i];
+                    leaf->all_entries[i]->MBR = r;
+                    leaf->all_entries[i]->LHV = h;
+                    for (int j = i + 1; j < 4; j++)
+                    {
+                        ENTRY temp2 = leaf->all_entries[j];
+                        leaf->all_entries[j] = temp;
+                        temp = temp2;
                     }
                 }
                 break;
             }
         }
+        AdjustTree(leaf->parent);
     }
-    else{
-        NODE leaf2=HandleOverflow(leaf,r);
+    else
+    {
+        NODE leaf2 = HandleOverflow(leaf, r);
+        NODE Np = leaf->parent;
+        if (leaf2 == NULL)
+            AdjustTree(Np);
+        else
+        {
+        }
     }
 }
